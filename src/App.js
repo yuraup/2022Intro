c804b6ed413dc352d495eef2808563717285c0b5
yuraup <o721@naver.com>
2022-05-10 02:54:52 +0900

    setting: router 설정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+
 import Main from './components/Main';
+import PortfolioJeong from './components/PortfolioJeong';
+import PortfolioYeon from './components/PortfolioYeon';
+import PortfolioYura from './components/PortfolioYura';
+import Soojeong from './components/Soojeong';
+import Soyeon from './components/Soyeon';
+import Yura from './components/Yura';
 
 function App() {
   return (
-    <div>
-      <header>
-        <div>
-          <Main />
-        </div>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/Portfolio/SooJeong" element={<PortfolioJeong />} />
+      <Route path="/Portfolio/Soyeon" element={<PortfolioYeon />} />
+      <Route path="/Portfolio/Yura" element={<PortfolioYura />} />
+      <Route path="/Yura" element={<Yura />} />
+      <Route path="/Soyeon" element={<Soyeon />} />
+      <Route path="/Soojeong" element={<Soojeong />} />
+    </Routes>
   );
 }
 
